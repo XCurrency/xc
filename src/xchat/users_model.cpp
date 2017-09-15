83f7eabed59ab57b5d22f99e2e987595a809590e
boa85 <belaev.oa85@gmail.com>
2017-09-16 00:35:32 +0600

    added class UserModel

---
+++ src/xchat/users_model.cpp
@@ -0,0 +1,131 @@
+#include "users_model.h"
+#include "../qt/addresstablemodel.h"
+
+#include <QDebug>
+
+//*****************************************************************************
+//*****************************************************************************
+UsersModel::UsersModel(QObject *parent) :
+        QAbstractListModel(parent), addressTableModel_(nullptr) {
+}
+
+//*****************************************************************************
+//*****************************************************************************
+// virtual
+int UsersModel::rowCount(const QModelIndex &parent) const {
+    Q_UNUSED(parent)
+    return static_cast<int>(items_.size());
+}
+
+//*****************************************************************************
+//*****************************************************************************
+// virtual
+QVariant UsersModel::data(const QModelIndex &index, int role) const {
+    auto row = static_cast<quint32>(index.row());
+    if (row >= items_.size()) {
+        return QVariant();
+    }
+
+    const Item &item = items_.at(row);
+
+    QString address = QString::fromStdString(item.addr);
+
+    QString label = labelForAddressWithAddress(address);
+
+    switch (role) {
+        case Qt::DisplayRole:
+            return (!item.isRead_ ? "* " : "") + label;
+        case Qt::ToolTipRole:
+            return label;
+        case roleAddress:
+            return address;
+        case roleLabel:
+            return labelForAddress(address);
+        case roleIsRead:
+            return item.isRead_;
+        default:
+            return QVariant();
+    }
+
+}
+
+//*****************************************************************************
+//*****************************************************************************
+void UsersModel::loadAddresses(const std::vector<std::string> &addresses) {
+    emit beginResetModel();
+    // items_ = addresses;
+    for (auto i = addresses.begin(); i != addresses.end(); ++i) {
+        items_.emplace_back(*i, true);
+    }
+    emit endResetModel();
+}
+
+//*****************************************************************************
+//*****************************************************************************
+void UsersModel::addAddress(const std::string &address, bool isNewMessage) {
+    emit beginResetModel();
+    items_.erase(std::remove(items_.begin(), items_.end(), address), items_.end());
+
+    items_.insert(items_.begin(), Item(address, !isNewMessage));
+    emit endResetModel();
+}
+
+//*****************************************************************************
+//*****************************************************************************
+void UsersModel::deleteAddress(const std::string &address) {
+
+    emit beginResetModel();
+    items_.erase(std::remove(items_.begin(), items_.end(), address), items_.end());
+    emit endResetModel();
+}
+
+//*****************************************************************************
+//*****************************************************************************
+//const std::vector<Message> & UsersModel::plainData() const
+//{
+//    return m_messages;
+//}
+
+//*****************************************************************************
+//*****************************************************************************
+void UsersModel::onRead(const QModelIndex &index) {
+
+    if (!index.isValid()) {
+        return;
+    }
+    if (index.row() > items_.size()) {
+        return;
+    }
+    items_[index.row()].isRead_ = true;
+    emit dataChanged(index, index);
+}
+
+//*****************************************************************************
+//*****************************************************************************
+void UsersModel::setAddressTableModel(AddressTableModel *model) {
+    addressTableModel_ = model;
+}
+
+//*****************************************************************************
+//*****************************************************************************
+QString UsersModel::labelForAddress(const QString &address) const {
+
+    static const QString emptyLabel = trUtf8("(no label)");
+
+    if (!addressTableModel_) {
+        return emptyLabel;
+    }
+    QString label = addressTableModel_->labelForAddress(address);
+    return label.isEmpty() ? emptyLabel : label;
+}
+
+//*****************************************************************************
+//*****************************************************************************
+QString UsersModel::labelForAddressWithAddress(const QString &address) const {
+    if (!addressTableModel_) {
+        return address;
+    }
+
+    QString label = addressTableModel_->labelForAddress(address);
+    return label.isEmpty() ? address : label + '(' + address + ')';
+}
---
+++ src/xchat/users_model.h
@@ -0,0 +1,76 @@
+#ifndef USERS_MODEL_H
+#define USERS_MODEL_H
+
+
+#include <QAbstractListModel>
+#include <QDateTime>
+#include <QString>
+
+#include <utility>
+#include <vector>
+
+class AddressTableModel;
+
+//*****************************************************************************
+//*****************************************************************************
+class UsersModel : public QAbstractListModel {
+Q_OBJECT
+
+public:
+    enum {
+        roleAddress = Qt::UserRole,
+        roleLabel,
+        roleIsRead
+    };
+
+    struct Item {
+        std::string addr;
+        bool isRead_;
+
+        explicit Item(std::string address,
+                      const bool isRead = false) : addr(std::move(address)), isRead_(isRead) {}
+
+        bool operator==(const std::string &address) const {
+            return addr == address;
+        }
+
+        bool operator==(const Item &item) const {
+            return addr == item.addr;
+        }
+    };
+
+public:
+    explicit UsersModel(QObject *parent = nullptr);
+
+public:
+    virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
+
+    virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+
+    void loadAddresses(const std::vector<std::string> &addresses);
+
+    void addAddress(const std::string &address, bool isNewMessage = true);
+
+    void deleteAddress(const std::string &address);
+
+    void setAddressTableModel(AddressTableModel *model);
+    // const std::vector<Message> & plainData() const;
+
+    QString labelForAddress(const QString &address) const;
+
+    QString labelForAddressWithAddress(const QString &address) const;
+
+signals:
+
+public slots:
+
+    void onRead(const QModelIndex &index);
+
+private:
+    std::vector<Item> items_;
+
+    AddressTableModel *addressTableModel_;
+};
+
+
+#endif // USERS_MODEL_H
